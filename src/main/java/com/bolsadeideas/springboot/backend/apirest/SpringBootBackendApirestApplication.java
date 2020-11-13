ce1c5b09fdcb07c3572797729d7fb02dbaaf10b6
Juan Carlos Melero <mrusuario0001@gmail.com>
2020-11-12 22:06:57 -0600

    duda

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/SpringBootBackendApirestApplication.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/SpringBootBackendApirestApplication.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootBackendApirestApplication implements CommandLineRunner{
+public class SpringBootBackendApirestApplication extends SpringBootServletInitializer  implements CommandLineRunner {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootBackendApirestApplication.class);
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApirestApplication.class, args);
 	}
